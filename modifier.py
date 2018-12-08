c55ae24f4c96e00e982d3d62b7da5358d631c217
Petra Gabriela <pgabriela@connect.ust.hk>
2018-12-08 22:28:12 +0800

    Replaced all invalid keys

--- modifier.py
+++ modifier.py
@@ -5,14 +5,19 @@ data = json.load(iF)
 
 
 for key in list(data['albums'].keys()):
-    if '$' in key:
-        data['albums'].pop(key, None)
+    if len(set('$#.[]/') & set(key)) > 0:
+        new_key = key.replace('$', 's').replace('#', ' ').replace('.', ' ').replace('[', '(').replace(']', ')').replace('/', ' ')
+        print(key)
+        print(new_key)
+        print('\n')
+        data['albums'][new_key] = data['albums'].pop(key)
 
 
 for key in list(data['clusters'].keys()):
     for x in range(len(data['clusters'][key]) - 1, -1, -1):
-        if '$' in data['clusters'][key][x]:
-            data['clusters'][key].pop(x)
+        if len(set('$#.[]/') & set(data['clusters'][key][x])) > 0:
+            new_val = data['clusters'][key][x].replace('$', 'S').replace('#', ' ').replace('.', ' ').replace('[', '(').replace(']', ')').replace('/', ' ')
+            data['clusters'][key][x] = new_val
 
 
 json.dump(data, open('data2.json', 'w'))
