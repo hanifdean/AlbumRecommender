56107d69e3b78d51ec1f29a7faef3b531f61562b
Petra Gabriela <pgabriela@connect.ust.hk>
2018-12-09 00:07:04 +0800

    Built the server for album similarities query

---
+++ server/server/routes/index.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.post('/cluster', function(req, res, next) {
+  res.render('cluster', { albumId: req.body['albumId'], clusterId: req.body['clusterId'] });
+});
+
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Album Recommender' });
+});
+
+module.exports = router;
